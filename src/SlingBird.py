347b1c088e34d8272c13a446aaf7692f88f0a63c
AppleTax <55190549+Apple-Tax@users.noreply.github.com>
2023-01-29 07:44:23 +0800

    added scene class

---
+++ src/SlingBird.py
@@ -0,0 +1,13 @@
+import pygame
+from scene import Scene
+
+class SlingBird:
+
+    def __init__(self):
+        pygame.init()
+        surface = pygame.display.set_mode((1280, 720))
+
+
+    def run(self, scene: Scene):
+        pass
+        
---
+++ src/scene/__init__.py
@@ -0,0 +1,1 @@
+from baseScene import Scene
